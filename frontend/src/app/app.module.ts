a7e3f3f51ffd4a3ec93960bf8351f743e8743d8a
Navonil Das <navneeladas@gmail.com>
2020-02-29 19:18:52 +0530

    Added list to show saved project

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/project/project.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-project',
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.css']
+})
+export class ProjectComponent implements OnInit {
+
+  constructor(private api: ApiService, private router: Router) { }
+  items:any[] = [];
+  ngOnInit() {
+    var token = window.localStorage.getItem("Token");
+    if (token)
+      this.api.getProjects(token).subscribe(v => {
+        this.items = v;
+      });
+    else {
+      this.router.navigate(["/login"]);
+    }
+  }
+
+}
